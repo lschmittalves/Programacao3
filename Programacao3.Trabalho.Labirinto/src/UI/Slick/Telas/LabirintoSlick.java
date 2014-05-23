64ae0da0d66ab102ebb438711ab9ec345c885bba
leonardo.alves <leonardo.alves@joic0410.catolicasc.org.br>
2014-05-22 21:02:18 -0300

    Adição da tela de importação do labirinto

--- Programacao3.Trabalho.Labirinto/src/UI/Slick/Telas/LabirintoSlick.java
+++ Programacao3.Trabalho.Labirinto/src/UI/Slick/Telas/LabirintoSlick.java
@@ -14,9 +14,14 @@ import UI.Slick.Framework.Components.ISlickComponent;
 import UI.Slick.LabirintoSlickMain;
 import java.util.LinkedList;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.Animation;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -27,7 +32,9 @@ import org.newdawn.slick.state.StateBasedGame;
  */
 public class LabirintoSlick extends BasicGameState implements IButtonListener {
 
+    private Animation sprite, up, down, left, right;
     private LinkedList<ISlickComponent> screenComponents;
+    private float x = 34f, y = 34f;
 
     public LabirintoSlick() {
         screenComponents = new LinkedList<>();
@@ -47,6 +54,20 @@ public class LabirintoSlick extends BasicGameState implements IButtonListener {
     @Override
     public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         gerarBackground();
+
+        Image[] movementUp = {new Image("data/sprites/stuart_bk_1.png"), new Image("data/sprites/stuart_bk_2.png"), new Image("data/sprites/stuart_bk_3.png"), new Image("data/sprites/stuart_bk_4.png"), new Image("data/sprites/stuart_bk_5.png"), new Image("data/sprites/stuart_bk_6.png"), new Image("data/sprites/stuart_bk_7.png")};
+        Image[] movementDown = {new Image("data/sprites/stuart_fr_1.png"), new Image("data/sprites/stuart_fr_2.png"), new Image("data/sprites/stuart_fr_3.png"), new Image("data/sprites/stuart_fr_4.png"), new Image("data/sprites/stuart_fr_5.png"), new Image("data/sprites/stuart_fr_6.png"), new Image("data/sprites/stuart_fr_7.png")};
+        Image[] movementLeft = {new Image("data/sprites/stuart_lft_1.png"), new Image("data/sprites/stuart_lft_2.png"), new Image("data/sprites/stuart_lft_3.png"), new Image("data/sprites/stuart_lft_4.png"), new Image("data/sprites/stuart_lft_5.png"), new Image("data/sprites/stuart_lft_6.png"), new Image("data/sprites/stuart_lft_7.png")};
+        Image[] movementRight = {new Image("data/sprites/stuart_rgt_1.png"), new Image("data/sprites/stuart_rgt_2.png"), new Image("data/sprites/stuart_rgt_3.png"), new Image("data/sprites/stuart_rgt_4.png"), new Image("data/sprites/stuart_rgt_5.png"), new Image("data/sprites/stuart_rgt_6.png"), new Image("data/sprites/stuart_rgt_7.png")};
+        int[] duration = {100, 100, 100, 100, 100, 100, 100};
+
+        up = new Animation(movementUp, duration, false);
+        down = new Animation(movementDown, duration, false);
+        left = new Animation(movementLeft, duration, false);
+        right = new Animation(movementRight, duration, false);
+
+        // Original orientation of the sprite. It will look right.
+        sprite = right;
     }
 
     @Override
@@ -55,11 +76,43 @@ public class LabirintoSlick extends BasicGameState implements IButtonListener {
         for (int i = 0; i < screenComponents.size(); i++) {
             screenComponents.get(i).draw();
         }
+        sprite.draw((int) x, (int) y);
     }
 
     @Override
-    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+        Input input = gc.getInput();
+        if (input.isKeyDown(Input.KEY_UP)) {
+            sprite = up;
 
+            sprite.update(delta);
+            // The lower the delta the slowest the sprite will animate.
+            y -= delta * 0.1f;
+
+        } else if (input.isKeyDown(Input.KEY_DOWN)) {
+            sprite = down;
+
+            sprite.update(delta);
+            y += delta * 0.1f;
+
+        } else if (input.isKeyDown(Input.KEY_LEFT)) {
+            sprite = left;
+
+            sprite.update(delta);
+            x -= delta * 0.1f;
+
+        } else if (input.isKeyDown(Input.KEY_RIGHT)) {
+            sprite = right;
+
+            sprite.update(delta);
+            x += delta * 0.1f;
+
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(LabirintoSlick.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
